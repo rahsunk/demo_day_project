f53da825ed412a366b40dd20f06e0d3e0abb1ae7
rahsunk <rtkf0522@gmail.com>
2022-08-16 14:52:58 -0400

    Edited home.js

--- home.js
+++ home.js
@@ -1,109 +1,90 @@
-let airQualityDiv=document.getElementById('airQuality')
-let airInput=document.getElementById('airInput')
-let airSubmitBtn=document.getElementById('airSubmit')
-let goodDiv = document.getElementById("good")
-let fairDiv = document.getElementById("fair")
-let moderateDiv = document.getElementById("moderate");
-let poorDiv = document.getElementById("poor")
-let veryPoorDiv = document.getElementById("very_poor")
+// The back to top button appears at the bottom of the home page, and takes the user back to the top of the page.
+const toTopBtn = document.querySelector("#back-to-top-btn");
+document.getElementById("back-to-top-btn").style.zIndex = "1"
 
-//   Upon clicking on the submit button, the value of the text box is for use in the latitude and longitude API
-airSubmitBtn.onclick=function(event){
-    event.preventDefault()
-    let airZip=airInput.value
-    getLatLon(airZip)
-}
+window.addEventListener("scroll", scrollFunction);
 
-const getLatLon=async(airZip='11692')=>{
-    const latLonAPI='https://api.openweathermap.org/geo/1.0/zip?zip='+airZip+',US&appid=8e61a85e3b57bba628d9d4ef2f4c94c7'
-    const latLonResponse=await fetch(latLonAPI)
-    const latLonData=await latLonResponse.json()
-    let lat=latLonData.lat
-    let lon=latLonData.lon
-  //   The returned latitude and longitude values will be used in the air quality API
-    getAirQuality(lat,lon,airZip)
-}
-
-const getAirQuality=async(lat,lon,airZip)=>{
-    const airQualityAPI=`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=8e61a85e3b57bba628d9d4ef2f4c94c7`
-    const airQualityResponse=await fetch(airQualityAPI)
-    const airQualityData=await airQualityResponse.json()
-  
-   let{list}=airQualityData
-    airQualityDiv.innerHTML=""
-    
-    let zipCodeHeader=document.createElement('h3')
-    zipCodeHeader.innerHTML=airZip
-    
-    let aqi=document.createElement('h1')
-    aqi.innerHTML=list[0].main.aqi
-
-    let severity=document.createElement('h3')
-    severity.style.fontSize="30px"
-    let description=document.createElement('p')
-    description.className="airDesc"
-    
-    let status=[
-      'Good',
-      'Fair',
-      'Moderate',
-      'Poor',
-      'Very Poor'
-    ]
-    
-    let advice=[
-      'Air pollution poses little to no risk.',
-      'Air pollution may cause risks to sensitive groups.',
-      'Members of sensitive groups may experience health effects. General groups are less likely to be affected.',
-      'Members of general groups may experience health effects. Sensitive groups may experience more serious effects.',
-      'Health Alert: The risk of health effects are greater for all groups.'
-    ]
-    
-    airQualityDiv.append(zipCodeHeader,aqi)
-    // The text and border color of each displayed index on the bottom is reset each time the function runs
-    airQualityDiv.style.color="black"
-    aqi.style.color="black"
-    goodDiv.style.border="2px black solid"
-    fairDiv.style.border="2px black solid"
-    moderateDiv.style.border="2px black solid"
-    poorDiv.style.border="2px black solid"
-    veryPoorDiv.style.border="2px black solid"
-    
-    // Depending on the returned AQI value, a specific condition and description is appended to the webpage, along with its corresponding index being highlighted in a green border.
-    if(aqi.innerHTML==1){
-      severity.append(status[0])
-      description.append(advice[0])
-      airQualityDiv.style.backgroundColor="lightgreen"
-      goodDiv.style.border = "7px solid green"
-    }
-    else if(aqi.innerHTML==2){
-      severity.append(status[1])
-      description.append(advice[1])
-      airQualityDiv.style.backgroundColor="yellow"
-      fairDiv.style.border = "7px solid green"
-    }
-    else if(aqi.innerHTML==3){
-      severity.append(status[2])
-      description.append(advice[2])
-      airQualityDiv.style.backgroundColor="orange"
-      moderateDiv.style.border = "7px solid green"
-    }
-    else if(aqi.innerHTML==4){
-      severity.append(status[3])
-      description.append(advice[3])
-      airQualityDiv.style.backgroundColor="red"
-      poorDiv.style.border = "7px solid green"
+function scrollFunction(){
+    if(window.pageYOffset>300){
+        toTopBtn.style.display = "block"
     }
     else{
-      severity.append(status[4])
-      description.append(advice[4])
-      airQualityDiv.style.backgroundColor="purple"
-      airQualityDiv.style.color="white"
-      aqi.style.color="white"
-      veryPoorDiv.style.border = "7px solid green"
+        toTopBtn.style.display = "none"
     }
-    airQualityDiv.append(severity,description)
 }
 
-getLatLon()
-// This initializes upon opening the page to display the current weather for default zip code: 11692
+toTopBtn.onclick = function(event){
+    event.preventDefault();
+    window.scrollTo(0,0);
+}
+
+
+// The following code below controls the text that is displayed underneath the sliding images when the media display is small enough.
+let carouselTxtMode=0
+
+const carouselButtonPrevList=document.getElementsByClassName("carousel-control-prev")
+const carouselButtonNextList=document.getElementsByClassName('carousel-control-next')
+const carouselButtonPrev=carouselButtonPrevList[0]
+const carouselButtonNext=carouselButtonNextList[0]
+const carouselTxt=document.getElementById('carouselTxt')
+    
+let carouselh5=document.createElement('h5')
+carouselh5.innerHTML="Air pollution"
+
+let carouselp=document.createElement('p')
+carouselp.innerHTML="4.5 million deaths were linked to outdoor air pollution in 2019"
+
+carouselTxt.append(carouselh5,carouselp)
+
+// Whenever the carousel previous or next buttons are clicked, the carousel mode increments by 1. If this number is even, it displays the air pollution text. If odd, it displays the climate change text.
+carouselButtonPrev.onclick=function(){
+    carouselTxtMode++
+    if(carouselTxtMode%2==0){
+        carouselTxt.innerHTML=""
+        
+        let carouselh5=document.createElement('h5')
+        carouselh5.innerHTML="Air pollution"
+        
+        let carouselp=document.createElement('p')
+        carouselp.innerHTML="4.5 million deaths were linked to outdoor air pollution in 2019"
+        
+        carouselTxt.append(carouselh5,carouselp)
+    }
+    else{
+        carouselTxt.innerHTML=""
+        
+        let carouselh5=document.createElement('h5')
+        carouselh5.innerHTML="Climate change"
+        
+        let carouselp=document.createElement('p')
+        carouselp.innerHTML="Between 2030 and 2050, over 250,000 additional deaths will be linked to climate change"
+        
+        carouselTxt.append(carouselh5,carouselp)
+    }
+}
+
+carouselButtonNext.onclick=function(){
+    carouselTxtMode++
+    if(carouselTxtMode%2==0){
+        carouselTxt.innerHTML=""
+        
+        let carouselh5=document.createElement('h5')
+        carouselh5.innerHTML="Air pollution"
+        
+        let carouselp=document.createElement('p')
+        carouselp.innerHTML="4.5 million deaths were linked to outdoor air pollution in 2019"
+        
+        carouselTxt.append(carouselh5,carouselp)
+    }
+    else{
+        carouselTxt.innerHTML=""
+        
+        let carouselh5=document.createElement('h5')
+        carouselh5.innerHTML="Climate change"
+        
+        let carouselp=document.createElement('p')
+        carouselp.innerHTML="Between 2030 and 2050, over 250,000 additional deaths will be linked to climate change"
+        
+        carouselTxt.append(carouselh5,carouselp)
+    }
+}
